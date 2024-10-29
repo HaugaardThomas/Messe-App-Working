5e28f7a807307a8719a94f8e3b0036ca5256e986
HaugaardThomas <legendenlomade@gmail.com>
2024-10-29 13:57:54 +0100

    metro config ændret

--- client/metro.config.js
+++ client/metro.config.js
@@ -4,4 +4,27 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add asset extensions required for Viro
+config.resolver = {
+  ...config.resolver,
+  assetExts: [
+    ...config.resolver.assetExts,
+    'obj', 'mtl', 'JPG', 'vrx', 'hdr', 'gltf', 'glb', 'bin', 'arobject', 'png',
+  ],
+  extraNodeModules: {
+    ...config.resolver.extraNodeModules,
+    '@reactvision/react-viro': require.resolve('@reactvision/react-viro'),
+  },
+};
+
+// Maintain default transformer options
+config.transformer = {
+  getTransformOptions: async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: false,
+    },
+  }),
+};
+
 module.exports = config;
